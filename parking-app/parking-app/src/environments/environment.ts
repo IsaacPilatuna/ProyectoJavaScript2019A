af5ae835bef66b32be006dc7499daaee417e7863
IsaacPilatuna <isapila1996@hotmail.com>
2019-05-25 17:08:38 -0500

    Configuración Firebase

---
+++ parking-app/parking-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyALZoTsLlA1RNmUFc-H2ZiXVGVXTu-4oGg",
+    authDomain: "proyectojs2019a.firebaseapp.com",
+    databaseURL: "https://proyectojs2019a.firebaseio.com",
+    projectId: "proyectojs2019a",
+    storageBucket: "proyectojs2019a.appspot.com",
+    messagingSenderId: "467194212074",
+    appId: "1:467194212074:web:2f10ae2bc5d12390"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
